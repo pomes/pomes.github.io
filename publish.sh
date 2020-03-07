cc64206fbf285cc5b28c86d72bf07abecbb24fb3
Duncan Dickinson <dedickinson@users.noreply.github.com>
2020-03-07 15:42:49 +1000

    More publishing work

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $(git status -s | wc -l) -gt 1 ]
+then
+    echo "Please commit all changes before publishing"
+    exit
+fi
+
 ./gradlew clean gitPublishCopy
 
 cd build/gitPublish
